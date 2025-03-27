d23d424530726e02cb6d213362a465115ffb41ba
prathameshdeogirkar <prathameshdeogirkar22485@gmail.com>
2025-03-27 23:36:02 +0530

    login page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import "./index.css"
 import Mycalender from './components/Mycalender'
 import Signup from './components/Signup'
 import 'remixicon/fonts/remixicon.css'
+import Login from './components/Login'
 
 const App = () => {
   return (
         <div className='flex '>
           <Mycalender/>
  
-         <Signup/>  
+         {/* <Signup/>   */}
+
+         <Login/>
         </div>
   )
 }
