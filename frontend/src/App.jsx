73e17f6b6207a0d937cdf77874a748bebf5eb70c
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-03-27 21:30:39 +0530

    add appinput

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./index.css"
 import Mycalender from './components/Mycalender'
+import AppInput from './components/AppInput'
 
 const App = () => {
   return (
         <div className='flex '>
           <Mycalender/>
+          
+          
         </div>
   )
 }
---
+++ frontend/src/components/AppInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppInput = ({placeholder,}) => {
+  return (
+    <input placeholder={placeholder}  className='h-5 p-4  border-4 shadow-black shadow-md '/>
+  )
+}
+
+export default AppInput
