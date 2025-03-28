6488e444a0e20d918e150b5b3f7ed390c6dd729c
prathameshdeogirkar <prathameshdeogirkar22485@gmail.com>
2025-03-28 11:37:05 +0530

    changes in login

--- frontend/src/components/Appbtn.jsx
+++ frontend/src/components/Appbtn.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
-const Appbtn = ({title="your tittle ",veriant="simple_Pink",onClick}) => {
-
- let veriants = {
-     simple_Blue:'bg-blue-500 hover:bg-blue-700',
-     simple_Red:'bg-red-500 hover:bg-red-700',
-     simple_Green:'bg-green-500 hover:bg-green-700',
-     simple_Yellow:'bg-yellow-500 hover:bg-yellow-700',
-     simple_Purple:'bg-purple-500 hover:bg-purple-700',
-     simple_Pink:'bg-pink-500 hover:bg-pink-700',
- }
+const Appbtn = ({ title = "Your Title", veriant = "simple_Pink", onClick }) => {
+  let veriants = {
+    simple_Blue: "bg-blue-500 hover:bg-blue-700",
+    simple_Red: "bg-red-500 hover:bg-red-700",
+    simple_Green: "bg-green-500 hover:bg-green-700",
+    simple_Yellow: "bg-yellow-500 hover:bg-yellow-700",
+    simple_Purple: "bg-purple-500 hover:bg-purple-700",
+    simple_Pink: "bg-pink-500 hover:bg-pink-700",
+  };
 
   return (
-         <button className={`text-2xl font-bold ${veriants[veriant]} w-full h-full myShadow`}>{title}</button>
-  )
-}
+    <button
+      className={`text-2xl font-bold ${veriants[veriant]} w-full h-full myShadow`}
+      onClick={onClick} 
+    >
+      {title}
+    </button>
+  );
+};
 
-export default Appbtn
+export default Appbtn;
--- frontend/src/components/Appinput.jsx
+++ frontend/src/components/Appinput.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const Appinput = ({placeholder,onChange}) => {
+const Appinput = ({ placeholder, onChange }) => {
   return (
-    <div className='w-full h-full text-black'>
-        <input className='h-full w-full outline-none myShadow text-xl px-2 font-medium  py-1' placeholder={placeholder}/>
+    <div className="w-full h-full text-black">
+      <input
+        className="h-full w-full outline-none myShadow text-xl px-2 font-medium py-1"
+        placeholder={placeholder}
+        onChange={onChange} 
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Appinput
+export default Appinput;
