778ef920042c8de62a293c38220e0a5d6a4ea733
prathameshdeogirkar <prathameshdeogirkar22485@gmail.com>
2025-03-31 17:25:14 +0530

    welcome popup added

--- frontend/src/components/UserIcon.jsx
+++ frontend/src/components/UserIcon.jsx
@@ -11,7 +11,7 @@ const UserIcon = () => {
  console.log(getCurrentUser());
   return (
     <>
-    <div className='h-12 w-44 bg-purple-300 myShadow absolute right-2 top-0 flex  items-center cursor-pointer overflow-hidden' onClick={()=>{
+    <div className='h-12 w-44 bg-purple-300 myShadow absolute right-2 top-0 flex  items-center cursor-pointer overflow-hidden z-0' onClick={()=>{
       setShowLogout(!showLogout)
     }}>
        <div className='h-[40px] w-[40px] ml-2 rounded-full bg-black border-2 border-black'>
